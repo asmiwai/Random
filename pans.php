e06f2e282a20b72015b04cdd9cd4342d4d6e9ff7
asmiwai <129585876+asmiwai@users.noreply.github.com>
2023-04-01 15:39:50 +0700

    Create pans.php

---
+++ pans.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var sites = [
+'https://shope.ee/7KQSMSMh8K?share_channel_code=2',
+'https://shope.ee/7f3Il820Mb?share_channel_code=2',
+'https://shope.ee/2py30LQmSv?share_channel_code=2',
+'https://shope.ee/AK43w9fgVE?share_channel_code=2',
+'https://shope.ee/5UyoBJJH8L?share_channel_code=2',
+'https://shope.ee/8KIzYYGpAe?share_channel_code=2',
+'https://shope.ee/9KBWkQgfDc?share_channel_code=2',
+'https://shope.ee/1q5Vog01MA?share_channel_code=2',
+'https://shope.ee/20Ow10ky0d?share_channel_code=2',
+'https://shope.ee/8pFG9eS93h?share_channel_code=2',
+'https://shope.ee/3Aav8js1RL?share_channel_code=2',
+'https://shope.ee/502SiT2RI2?share_channel_code=2',
+'https://shope.ee/3pqY95tx0S?share_channel_code=2',
+'https://shope.ee/9p7iYQk71w?share_channel_code=2'];
+window.location.replace(sites[Math.floor(Math.random()*sites.length)]);
+</script>
+</body>
+</html>
